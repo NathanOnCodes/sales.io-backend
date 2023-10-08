07eb79afe4f9062e39aa7deaa9b90f47b47322d8
NathanOnCodes <contatonathancf@gmail.com>
2023-10-08 15:10:11 -0300

    feat: conexao com banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'secret',
+      database: 'salespostgres',
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
