923290fa0650ef34e83ee703235dfbf011fbdc83
NathanOnCodes <contatonathancf@gmail.com>
2023-10-10 16:29:20 -0300

    fix: conexao com banco de dados com modulo do banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['.env.development.local'],
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'admin',
-      password: 'secret',
-      database: 'salespostgres',
-      entities: [],
-      synchronize: true,
-      autoLoadEntities: true,
-    }),
     UserModule,
+    DatabaseModule,
   ],
   controllers: [],
   providers: [],
